911faac05347b83f8c8ef2e28328dc316727fbc1
Giuseppe Averta <g.averta3@gmail.com>
2026-03-25 14:36:55 +0100

    lezione del 25/03

---
+++ dao/dbConnect.py
@@ -0,0 +1,20 @@
+import mysql.connector
+
+class DBConnect:
+
+    @classmethod
+    def getConnection(cls):
+        try:
+            cnx = mysql.connector.connect(
+                user = "root",
+                password = "rootroot",
+                host = "127.0.0.1",
+                database = "sw_gestionale"
+            )
+            return cnx
+
+        except mysql.connector.Error as err:
+            print("Non riesco a collegarmi al db")
+            print(err)
+            return None
+
